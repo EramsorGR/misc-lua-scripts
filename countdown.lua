0330d53e54e921dcca9a03947b0a65379497ebb0
Nicholas S <eramsor1@gmail.com>
2020-04-03 17:25:02 +0300

    Update and rename basic timer.lua to countdown.lua

--- countdown.lua
+++ countdown.lua
@@ -4,6 +4,8 @@ repeat
 	local mininutes = math.floor(timer/60) --Get the minutes 
 	local seconds = math.floor(((timer/60) - min) * 60) --Get the seconds from these minutes
 	print(mininutes .. seconds)
+	--alternative position for line 3
 	wait(1)
+	--alternative position for line 3
 until timer <= 0 --Stop the timer when it reaches 0 
 print('END')
