b311f3b72e9d4a064db272599c259308f1bf587f
Nicholas S <eramsor1@gmail.com>
2020-04-03 17:19:53 +0300

    Init

---
+++ basic timer.lua
@@ -0,0 +1,9 @@
+local timer = 1800 --Provide the seconds for the timer
+repeat 
+	timer = timer - 1 --Calculate the next value, you can place this before the loop end as well 
+	local mininutes = math.floor(timer/60) --Get the minutes 
+	local seconds = math.floor(((timer/60) - min) * 60) --Get the seconds from these minutes
+	print(mininutes .. seconds)
+	wait(1)
+until timer <= 0 --Stop the timer when it reaches 0 
+print('END')
